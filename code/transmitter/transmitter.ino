a866909bdb1dc4371c4b1cf60a23920156505e14
joshHuang02 <52260865+joshHuang02@users.noreply.github.com>
2023-11-16 15:18:28 -0500

    new transmitter file from other group

--- code/transmitter/transmitter.ino
+++ code/transmitter/transmitter.ino
@@ -1,29 +1,27 @@
-/*
-* Arduino Wireless Communication Tutorial
-*     Example 1 - Transmitter Code
-*                
-* by Dejan Nedelkovski, www.HowToMechatronics.com
-* 
-* Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
-*/
-
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
 
-RF24 radio(7, 8); // CE, CSN
-
+RF24 radio(7, 8);  // CE, CSN
 const byte address[6] = "00001";
+const int buttPin = 6;
 
 void setup() {
   radio.begin();
   radio.openWritingPipe(address);
   radio.setPALevel(RF24_PA_MIN);
   radio.stopListening();
+  pinMode(buttPin, INPUT);
 }
 
 void loop() {
-  const char text[] = "Hello World";
-  radio.write(&text, sizeof(text));
-  delay(1000);
+  int buttState = digitalRead(buttPin);
+  if (buttState == HIGH) {
+    const char text[] = "OFF";
+    radio.write(&text, sizeof(text));
+  } else {
+    const char text[] = "ON";
+    radio.write(&text, sizeof(text));
+  }
+  delay(1000); // Adjust delay as needed
 }
